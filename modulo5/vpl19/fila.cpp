3ba0d680ab985a26d3974ceee2de2bafa79c2aba
cassohir <cassio.hir@gmail.com>
2022-07-01 14:55:38 -0300

    +1

---
+++ modulo5/vpl19/fila.cpp
@@ -0,0 +1,30 @@
+#include "fila.h"
+
+struct No {
+  int chave;
+  No* proximo;
+};
+
+Fila::Fila() {
+  // TODO
+}
+
+void Fila::Inserir(int k) {
+  // TODO
+}
+
+void Fila::RemoverPrimeiro() {
+  // TODO
+}
+
+int Fila::primeiro() const {
+  return 0; // TODO
+}
+
+int Fila::ultimo() const {
+  return 0; // TODO
+}
+
+int Fila::tamanho() const {
+  return 0; // TODO
+}
